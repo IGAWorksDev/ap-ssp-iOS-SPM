a3520f5654b1619f176dbb378fd68825aba7527e
Odin-AP <odin.song@adpopcorn.com>
2025-04-15 13:45:14 +0900

    [add] AdPopcornSSPSDK SPM xcframework 추가

---
+++ xcframework/AdPopcornSSPSDK.xcframework/ios-arm64/AdPopcornSSPSDK.framework/Headers/AdPopcornSSPSDK.h
@@ -0,0 +1,18 @@
+//
+//  AdPopcornSSPSDK.h
+//  AdPopcornSSPSDK
+//
+//  Created by Odin.송황호 on 12/9/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdPopcornSSPSDK.
+FOUNDATION_EXPORT double AdPopcornSSPSDKVersionNumber;
+
+//! Project version string for AdPopcornSSPSDK.
+FOUNDATION_EXPORT const unsigned char AdPopcornSSPSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdPopcornSSPSDK/PublicHeader.h>
+
+
