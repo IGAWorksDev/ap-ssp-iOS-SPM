b7ac35762d9cf2024be50a36409a655927b45613
Odin-AP <odin.song@adpopcorn.com>
2025-04-15 18:29:09 +0900

    [fix] APSSPSDKMediationAdFit 제거

--- Package.swift
+++ Package.swift
@@ -13,39 +13,14 @@ let package = Package(
         .library(
             name: "AdPopcornSSPSDK",
             targets: ["AdPopcornSSPSDK"]),
-        
-        .library(
-            name: "APSDKMediationAdFit",
-            targets: ["APSDKMediationAdFitWrapper"]), // <- Wrapper 타겟을 외부로 노출
             
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-//        .package(url: "https://github.com/adfit/adfit-spm.git", from: "3.18.6"),
-        .package(name: "AdFitSDK",
-                 url: "https://github.com/adfit/adfit-spm.git",
-                 from: "3.18.6")
-        
-    ],
+    
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "AdPopcornSSPSDK",
                       path: "xcframework/AdPopcornSSPSDK.xcframework"),
         
-        // AdFit mediation 모듈 본체 (추가 UI 로직이 없으면 이걸로 충분)
-        .binaryTarget(name: "APSDKMediationAdFit",
-                      path: "xcframework/Mediation/APSDKMediationAdFit.xcframework"),
-
-        // Wrapper 타겟: 외부에서 사용하는 진짜 타겟은 얘!
-        .target(
-            name: "APSDKMediationAdFitWrapper",
-            dependencies: ["AdPopcornSSPSDK",
-                           "APSDKMediationAdFit",
-                .product(name: "AdFitSDK",
-                         package: "AdFitSDK"),
-            ],
-            path: "xcframework/APSDKMediationAdFitWrapper"
-        ),
     ]
 )
--- xcframework/APSDKMediationAdFitWrapper/APSDKMediationAdFitWrapper.m
+++
@@ -1,9 +0,0 @@
-//
-//  APSDKMediationAdFitWrapper.m
-//  AdPopcornSSP
-//
-//  Created by Odin.송황호 on 4/15/25.
-//
-
-#import <Foundation/Foundation.h>
-#import "APSDKMediationAdFitWrapper.h"
